9a3ae5f75b312fd167eb5b31f653023de1097f88
Adriano Eduardo Trentin Júnior <adriano.eduardo@inatel.br>
2023-03-27 15:32:06 -0300

    [ADD] : Create table to persist user of system.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,11 +11,61 @@ export class AppComponent {
   title = 'AngularExample';
 
   public user: User = {} as User;
+  public arrayUsers: Array<User> = [];
+  public insert: Boolean = true;
 
-  ngOnInit(){
+  show() {
+    console.log(this.user)
   }
 
-  show(){
-    console.log(this.user)
+  registerUser() {
+    if (this.insert)
+    {
+      if (this.isEmptyUser())
+      {
+        this.insertUser(this.user);
+        console.log("Registered Success ! " + this.arrayUsers);
+      }
+      else
+      {
+        console.error("User name and address are empty");
+      }
+    }
+    else
+    {
+      this.deleteUser(this.user);
+      this.insertUser(this.user);
+      console.log("Updated Success ! " + this.arrayUsers);
+    }
+  }
+
+  cleanUser() {
+    this.user = {} as User;
+    this.insert = true;
+  }
+
+  insertUser(user: User){
+    this.arrayUsers.push(user);
+    this.cleanUser();
+  }
+
+  updateUser(user: User) {
+    this.user.name = user.name;
+    this.user.address = user.address;
+    this.user.city = user.city;
+    this.user.email = user.email;
+    this.user.state = user.state;
+
+    this.insert = false;
+  }
+
+  deleteUser(user: User)
+  {
+    let index = this.arrayUsers.findIndex(x => x.email == this.user.email);
+    this.arrayUsers.splice(index, 1);
+  }
+
+  isEmptyUser(): Boolean {
+    return this.user.name != "" && this.user.address != "";
   }
 }
